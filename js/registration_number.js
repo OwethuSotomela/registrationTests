a9c16dfd8227e1e7dff4ff8cb2c459e27e1708e7
OwethuSotomela <owethusotomela@gmail.com>
2021-06-01 14:31:30 +0200

    first commit

---
+++ js/registration_number.js
@@ -0,0 +1,99 @@
+var inputTextElement = document.querySelector(".townName");
+var addButtonElement = document.querySelector(".addButton");
+var radioButtonsElement = document.querySelector(".itemTypeRadio1");
+var displayElement = document.querySelector(".outPut");
+var errorsElement = document.querySelector(".errors");
+var showAllElement = document.querySelector(".showAllButton");
+var showElement = document.querySelector(".showButton");
+var resetBtnElement = document.querySelector(".resetButton");
+
+var regStored = [];
+
+if (localStorage['plates']) {
+    regStored = JSON.parse(localStorage.getItem('plates'))
+} else {
+    regStored = [];
+}
+
+let regi = Registration(regStored)
+
+dsiplayFun(regi.getRegi())
+
+function dsiplayFun(argPassed) {
+
+    let element = document.getElementById("myList");
+    while (element.firstChild) {
+        element.removeChild(element.firstChild);
+    }
+
+    for (var i = 0; i < argPassed.length; i++) {
+        var node = document.createElement("li");
+        var textnode = document.createTextNode(argPassed[i]);
+        node.appendChild(textnode);
+        element.appendChild(node);
+    }
+}
+function setOut() {
+    setTimeout(function () {
+        errorsElement.innerHTML = ''
+    }, 3000)
+}
+function addRegistration() {
+    var inputText = inputTextElement.value
+
+    if (inputText !== "") {
+        if (regi.setRegi(inputText)) {
+            localStorage.setItem('plates', JSON.stringify(regi.getRegi()));
+
+            dsiplayFun(regi.getRegi())
+            errorsElement.classList.remove("errors")
+            errorsElement.classList.add("green")
+            errorsElement.innerHTML = regi.getMessage()
+        } else {
+            errorsElement.classList.remove("green")
+            errorsElement.classList.add("errors")
+            errorsElement.innerHTML = regi.getMessage()
+        }
+    } else {
+        errorsElement.classList.remove("green")
+        errorsElement.classList.add("errors")
+        errorsElement.innerHTML = "Please enter a valid registration number!"
+    }
+    inputTextElement.value = ''
+    setOut()
+}
+function forTown() {
+    var radioBtnChecked = document.querySelector("input[name='itemType1']:checked");
+    if (radioBtnChecked) {
+        radioBtnChecked = radioBtnChecked.value
+        regi.regPlate(radioBtnChecked)
+        var array = regi.getFilterTown()
+        if (array.length != 0) {
+            dsiplayFun(array)
+        } else {
+            errorsElement.classList.remove("green")
+            errorsElement.classList.add("errors")
+            errorsElement.innerHTML = "No registration numbers for this town!"
+            dsiplayFun(array)
+        }
+
+    } else {
+        errorsElement.classList.remove("green")
+        errorsElement.classList.add("errors")
+        errorsElement.innerHTML = "Select the town to show the registration numbers for!"
+    } setOut()
+}
+
+function removeCars() {
+
+    errorsElement.classList.remove("errors")
+    errorsElement.classList.add("green")
+    errorsElement.innerHTML = "Local storage will be cleared in 5 second!"
+    setTimeout(function () {
+        localStorage.clear()
+        location.reload()
+    }, 5000)
+}
+showElement.addEventListener('click', forTown)
+resetBtnElement.addEventListener('click', removeCars)
+addButtonElement.addEventListener('click', addRegistration)
---
+++ js/registration_number.widget.js
@@ -0,0 +1,56 @@
+function Registration(local) {
+
+    var regList = local;
+    var message = "";
+    var filterTown = []
+
+    function setRegi(carReg){
+
+        carReg = carReg.toUpperCase();
+
+        var regx = /[A-Z]{2}\s[0-9]{6}$/.test(carReg)
+        var regx1 = /[A-Z]{2}\s[0-9]{3}\s[0-9]{3}$/.test(carReg)
+        var regx2 = /[A-Z]{2}\s[0-9]{3}\-[0-9]{3}$/.test(carReg)
+
+        if(!regx && !regx1 && !regx2){
+            message = "Format doesn't match the required!"
+            return false;
+        } else{
+            if (!regList.includes(carReg)){
+                regList.push(carReg)
+                message = "Car registration added successfully!";
+                return true;
+            } else {
+                message = "Car registration number already entered!";
+                return false;
+            }
+        }
+    }
+    function regPlate(regPlate){
+        filterTown = []
+        for(var i=0; i<regList.length; i++){
+            if(regList[i].startsWith(regPlate)){
+                filterTown.push(regList[i])
+            }
+        }
+    }
+    function getFilterTown(){
+        return filterTown;
+    }
+    function getRegi() {
+        return regList;
+    }
+    function getMessage(){
+        return message;
+    }
+    return {
+        Registration,
+        setRegi,
+        regPlate,
+        getRegi,
+        getMessage,
+        getFilterTown,
+    }
+}
+
+
